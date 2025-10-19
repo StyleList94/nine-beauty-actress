0204d244d42c9776ab3b9ac2f797aa5912fc7136
StyleList94 <43318609+StyleList94@users.noreply.github.com>
2025-10-19 11:50:30 +0900

    chore: remove deprecated hook

--- lib/main.ts
+++ lib/main.ts
@@ -6,7 +6,6 @@ import { Header, HeaderProps } from './components/header';
 import { MainContainer, MainContainerProps } from './components/main-container';
 
 import useDebounce from './hooks/use-debounce';
-import useEffectEvent from './hooks/use-effect-event';
 import useMounted from './hooks/use-mounted';
 import useVirtualScroll from './hooks/use-virtual-scroll';
 
@@ -18,8 +17,6 @@ export {
   Header,
   MainContainer,
   useDebounce,
-  // eslint-disable-next-line @typescript-eslint/no-deprecated
-  useEffectEvent,
   useMounted,
   useVirtualScroll,
 };
